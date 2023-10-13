e377ae36427cac2ff86ca54f8f0f61d71d2c240c
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2023-10-13 12:55:53 +0300

    post-launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </ClerkProvider>
   )
 }
-
-/* 
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" /><link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-<link rel="manifest" href="/favicon/site.webmanifest" />
-*/
